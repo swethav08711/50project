35a8490425ff6b95e65a46b07678eefe63b00880
swethav08711 <shwe08711@gmail.com>
2021-10-05 19:17:02 +0530

    script of random pick

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -6,6 +6,12 @@ const textarea = document.getElementById("textarea")
 const tagsEl = document.getElementById("tags")
 textarea.addEventListener("keyup", event => {
   createtags(event.target.value)
+  if (event.key === "Enter") {
+    setTimeout(() => {
+      event.target.value = ""
+    }, 10)
+    randomSelect()
+  }
 })
 function createtags(input) {
   const tags = input
@@ -21,3 +27,37 @@ function createtags(input) {
     tagsEl.append(t)
   })
 }
+
+function randomSelect() {
+  const times = 30
+  const int = 100
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+    //hoghlight atgs
+    highlightTag(randomTag)
+    //unhighlight
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    }, int)
+  }, int)
+  setTimeout(() => {
+    clearInterval(interval)
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      //hoghlight atgs
+      highlightTag(randomTag)
+    }, int)
+  }, int * times)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag")
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+function highlightTag(tag) {
+  tag.classList.add("highlight")
+}
+function unhighlightTag(tag) {
+  tag.classList.remove("highlight")
+}
+//know when the enter is pressed we a random select
