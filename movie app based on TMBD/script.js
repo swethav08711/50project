fc3d364a7eebd1a0d2129e0e4ea78da56562af73
swethav08711 <shwe08711@gmail.com>
2021-10-07 12:52:35 +0530

    script done

--- movie app based on TMBD/script.js
+++ movie app based on TMBD/script.js
@@ -4,86 +4,84 @@ const api_url = Base_url + "/discover/movie?sort_by=popularity.desc&" + API_key
 const img_url = "https://image.tmdb.org/t/p/w500"
 const search_url = Base_url + "/search/movie?" + API_key
 
-const generes = {
-  genres: [
-    {
-      id: 28,
-      name: "Action",
-    },
-    {
-      id: 12,
-      name: "Adventure",
-    },
-    {
-      id: 16,
-      name: "Animation",
-    },
-    {
-      id: 35,
-      name: "Comedy",
-    },
-    {
-      id: 80,
-      name: "Crime",
-    },
-    {
-      id: 99,
-      name: "Documentary",
-    },
-    {
-      id: 18,
-      name: "Drama",
-    },
-    {
-      id: 10751,
-      name: "Family",
-    },
-    {
-      id: 14,
-      name: "Fantasy",
-    },
-    {
-      id: 36,
-      name: "History",
-    },
-    {
-      id: 27,
-      name: "Horror",
-    },
-    {
-      id: 10402,
-      name: "Music",
-    },
-    {
-      id: 9648,
-      name: "Mystery",
-    },
-    {
-      id: 10749,
-      name: "Romance",
-    },
-    {
-      id: 878,
-      name: "Science Fiction",
-    },
-    {
-      id: 10770,
-      name: "TV Movie",
-    },
-    {
-      id: 53,
-      name: "Thriller",
-    },
-    {
-      id: 10752,
-      name: "War",
-    },
-    {
-      id: 37,
-      name: "Western",
-    },
-  ],
-}
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+]
 
 const main = document.getElementById("main")
 const form = document.getElementById("form")
@@ -96,6 +94,37 @@ function getmovies(url) {
       showmoives(data.results)
       console.log(data.results)
     })
+}
+const tagsEl = document.getElementById("tags")
+
+var selectedGenre = []
+setGenre()
+function setGenre() {
+  tagsEl.innerHTML = ""
+  genres.forEach(genre => {
+    const t = document.createElement("div")
+    t.classList.add("tag")
+    t.id = genre.id
+    t.innerText = genre.name
+    t.addEventListener("click", () => {
+      if (selectedGenre.length === 0) {
+        selectedGenre.push(genre.id)
+      } else {
+        if (selectedGenre.includes(genre.id)) {
+          selectedGenre.forEach((id, idx) => {
+            if (id == genre.id) {
+              selectedGenre.splice(idx, 1)
+            }
+          })
+        } else {
+          selectedGenre.push(genre.id)
+        }
+      }
+      console.log(selectedGenre)
+      getmovies(api_url + "&with_genres=" + encodeURI(selectedGenre.join(",")))
+    })
+    tagsEl.append(t)
+  })
 }
 
 function showmoives(data) {
