9928a29274ee95810197fb3995a2eb8696cbe8e2
swethav08711 <shwe08711@gmail.com>
2021-10-07 12:33:06 +0530

    script done

--- movie app based on TMBD/script.js
+++ movie app based on TMBD/script.js
@@ -3,6 +3,88 @@ const Base_url = "https://api.themoviedb.org/3"
 const api_url = Base_url + "/discover/movie?sort_by=popularity.desc&" + API_key
 const img_url = "https://image.tmdb.org/t/p/w500"
 const search_url = Base_url + "/search/movie?" + API_key
+
+const generes = {
+  genres: [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentary",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Family",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 36,
+      name: "History",
+    },
+    {
+      id: 27,
+      name: "Horror",
+    },
+    {
+      id: 10402,
+      name: "Music",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 10770,
+      name: "TV Movie",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
+  ],
+}
+
 const main = document.getElementById("main")
 const form = document.getElementById("form")
 const search = document.getElementById("search")
